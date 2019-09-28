457f7d51ac48d3ba4758e5952fb4667cc709356c
Matt Plichta <bigcatplichta@gmail.com>
2019-09-28 00:46:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,9 +6,14 @@ def create_project_hash
   html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
   binding.pry 
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
 end
 
-projects = {}
+
 
 create_project_hash
 
